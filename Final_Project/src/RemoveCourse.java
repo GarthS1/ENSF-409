8509112d5f0e202551ada9e56591fb6446001d32
Jiho Kim <46981980+Jiho-K@users.noreply.github.com>
2020-04-08 15:24:21 -0600

    Add files via upload

---
+++ Final_Project/src/RemoveCourse.java
@@ -0,0 +1,102 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+/**
+ * This class creates a GUI for removing course from a student's currently enrolled courses.
+ * @author jiho
+ *
+ */
+class RemoveCourse {
+	MenuGUI menu;
+	JFrame frame;
+	JPanel dropDownPanel;
+	JPanel buttonPanel;
+	JPanel combinedPanel;
+	
+	/**
+	 * Should contain the list of courses the student is currently enrolled in.
+	 */
+	String [] temp;
+	
+	/**
+	 * A drop-down menu displaying what the courses the student is currently enrolled in.
+	 */
+	JComboBox<String> dropDownMenu;
+	
+	/**
+	 * Displays instruction on what the user should do onto the GUI.
+	 */
+	JLabel text;
+	
+	JButton removeButton;
+	JButton cancelButton;
+	
+	RemoveCourse(MenuGUI m) {
+		menu = m;
+		frame = new JFrame();
+		RemoveCourseListener listener = new RemoveCourseListener();
+		
+		dropDownPanel = new JPanel();
+		//change this with information from the database.
+		String [] temp = {"a", "b", "c"};
+		dropDownMenu = new JComboBox<String>(temp);
+		text = new JLabel("What course would you like to remove?");
+		
+		removeButton = new JButton("Remove");
+		cancelButton = new JButton("Cancel");
+		
+		removeButton.addActionListener(listener);
+		cancelButton.addActionListener(listener);
+		
+		dropDownPanel.setLayout(new BoxLayout(dropDownPanel, BoxLayout.PAGE_AXIS));
+		dropDownPanel.add(text);
+		dropDownPanel.add(Box.createVerticalStrut(10));
+		dropDownPanel.add(dropDownMenu);
+		dropDownPanel.setBorder(BorderFactory.createEmptyBorder(0, 0, 10, 0));
+		
+		buttonPanel = new JPanel();
+		buttonPanel.add(removeButton);
+		buttonPanel.add(cancelButton);
+		
+		combinedPanel = new JPanel();
+		combinedPanel.setLayout(new BoxLayout(combinedPanel, BoxLayout.PAGE_AXIS));
+		combinedPanel.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
+		combinedPanel.add(dropDownPanel);
+		combinedPanel.add(buttonPanel);
+		
+		frame.add(combinedPanel);
+		frame.pack();
+		
+		frame.setVisible(true);
+	}
+	
+	/**
+	 * This class handles what happens when a button is clicked. When "Cancel" is clicked, the frame closes.
+	 * When the "Remove" button is clicked, the program goes into the database and deletes the course from the student.
+	 * @author jiho
+	 *
+	 */
+	private class RemoveCourseListener implements ActionListener {
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if(e.getSource() == removeButton) {
+				 JOptionPane.showConfirmDialog(null, "Help me end my suffering...",
+							"suffering", JOptionPane.YES_NO_OPTION);
+			} else if(e.getSource() == cancelButton) {
+				frame.dispose();
+				menu.frame.setVisible(true);
+			}
+		}
+	}
+}
