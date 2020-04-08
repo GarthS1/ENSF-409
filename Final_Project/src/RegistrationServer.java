abe6ca872f82c92915a59d6523fe941452b43f2d
Garth Slaney <45348251+GarthS1@users.noreply.github.com>
2020-04-08 15:31:35 -0600

    Update RegistrationServer.java

--- Final_Project/src/RegistrationServer.java
+++ Final_Project/src/RegistrationServer.java
@@ -56,13 +56,17 @@ public class RegistrationServer {
 				System.out.println("Connection accepted by server");
 				socketIn = new BufferedReader ( new InputStreamReader(aSocket.getInputStream()) );
 				socketOut = new PrintWriter ( aSocket.getOutputStream(), true);
-				Student st = stu.searchStudent(Integer.parseInt(socketIn.readLine()));  //makes sure valid ID is entered before running
-				if(st != null) {
-					RegistrationController reg = new RegistrationController(socketIn, socketOut, cat, st);   //constructs a runnable 
-					pool.execute(reg);																																			 //executes the runnable in a thread pool
-				}
-				else {
-					socketOut.print("No student with the ID eneter matched. Please try again");							//error message if no ID matches 
+				Student st = null;
+				while(st == null) {
+					st = stu.searchStudent(Integer.parseInt(socketIn.readLine()));  //makes sure valid ID is entered before running
+					if(st != null) {
+						socketOut.print("Student found. Logging in.");
+						RegistrationController reg = new RegistrationController(socketIn, socketOut, cat, st);   //constructs a runnable 
+						pool.execute(reg);																																			 //executes the runnable in a thread pool
+					}
+					else {
+						socketOut.print("No student with the ID eneter matched. Please try again");							//error message if no ID matches 
+					}
 				}
 			}
 			}catch(IOException e) {
