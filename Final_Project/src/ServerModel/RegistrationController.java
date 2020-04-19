a9b7df4523811fe806c7503e3419266e03dd285b
Garth Slaney <45348251+GarthS1@users.noreply.github.com>
2020-04-19 13:52:25 -0600

    Update RegistrationController.java

--- Final_Project/src/ServerModel/RegistrationController.java
+++ Final_Project/src/ServerModel/RegistrationController.java
@@ -61,38 +61,40 @@ public class RegistrationController implements Runnable{
 		boolean exit = false;			//exit condition for the loop
 		while(!exit) {
 			try {
-				action = socketIn.readLine();
-				switch(action) {
-				case "search catalogue courses":
-					String course = socketIn.readLine();
-					int courseId = Integer.parseInt(socketIn.readLine());
-					Course courseSearched = cat.searchCat(course, courseId);
-					socketOut.println(courseSearched);
-					break;
-				case "add course to student course":
-				  Registration addedCourse = new Registration ();
-					String course1 = socketIn.readLine();
-					int courseId1 = Integer.parseInt(socketIn.readLine());
-					Course courseSearched1 = cat.searchCat(course1, courseId1);
-					int section = Integer.parseInt(socketIn.readLine()) - 1; //need to subtract one to get effective address 
-				  addedCourse.completeRegistration(st, courseSearched1.getCourseOfferingAt(section));
-					break;
-				case "remove course from student course":
-					socketOut.println(st.printCourses());
-					int removeId = Integer.parseInt(socketIn.readLine()); 
-					st.removeRegistration(removeId);
-					break;
-				case "view All courses in catalog":
-					socketOut.print(cat);
-					break;
-				case "view all courses taken by student":
-					socketOut.print(st.printCourses());
-					break;
-				case "quit":
-					exit = true;
-					break;
-				default:
-					socketOut.print("Invalid input enetered. Please enter a different input.");
+				if(socketIn.ready()) {
+					action = socketIn.readLine();
+					switch(action) {
+					case "search catalogue courses":
+						String course = socketIn.readLine();
+						int courseId = Integer.parseInt(socketIn.readLine());
+						Course courseSearched = cat.searchCat(course, courseId);
+						socketOut.println(courseSearched);
+						break;
+					case "add course to student course":
+					  Registration addedCourse = new Registration ();
+						String course1 = socketIn.readLine();
+						int courseId1 = Integer.parseInt(socketIn.readLine());
+						Course courseSearched1 = cat.searchCat(course1, courseId1);
+						int section = Integer.parseInt(socketIn.readLine()) - 1; //need to subtract one to get effective address 
+					  addedCourse.completeRegistration(st, courseSearched1.getCourseOfferingAt(section));
+						break;
+					case "remove course from student course":
+						socketOut.println(st.printCourses());
+						int removeId = Integer.parseInt(socketIn.readLine()); 
+						st.removeRegistration(removeId);
+						break;
+					case "view All courses in catalog":
+						socketOut.print(cat);
+						break;
+					case "view all courses taken by student":
+						socketOut.print(st.printCourses());
+						break;
+					case "quit":
+						exit = true;
+						break;
+					default:
+						socketOut.print("Invalid input enetered. Please enter a different input.");
+					}
 				}
 			} catch (IOException e) {
 				e.printStackTrace();
